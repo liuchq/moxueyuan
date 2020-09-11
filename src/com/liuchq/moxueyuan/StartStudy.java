abe62cb8002d5ae86462f5e2fbc0dfe2c41531a4
liuchq <43339366+liuchq@users.noreply.github.com>
2020-09-11 11:04:50 +0800

    Add files via upload
    update

--- src/com/liuchq/moxueyuan/StartStudy.java
+++ src/com/liuchq/moxueyuan/StartStudy.java
@@ -1,6 +1,7 @@
 package com.liuchq.moxueyuan;
 
 import java.io.*;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -11,25 +12,36 @@ import java.util.Map;
  */
 public class StartStudy {
 
+    /**
+     * 标志课程是否已经学习完毕
+     */
+    public static Boolean isStudyFinish = false;
+
     public static void main(String[] args){
         MoXueYuanLog.setLog("开始学习---本次学习设定的运行时间为"+MoXueYuanConstant.RUN_TIME+"秒",null);
-        try {
-            //获取全部课程ID
-            List<Map<String,String>> mapList = MoXueYuanStudy.getCourse();
-            //todo  是否此处可以用多线程解决，然后请求速度加快
-            for (Map<String, String> map : mapList) {
-                //开始课程学习
-                MoXueYuanLog.setLog("---本次学习课程ID："+map.get(MoXueYuanEnum.COURSE_ID_NAME.getValue())+","+map.get(MoXueYuanEnum.CHAPTER_ID_NAME.getValue()),null);
-                MoXueYuanStudy.courseStudyMethod(map.get(MoXueYuanEnum.COURSE_ID_NAME.getValue()),
-                        map.get(MoXueYuanEnum.CHAPTER_ID_NAME.getValue()));
+        LocalDateTime endTime = LocalDateTime.now().plusSeconds(Long.valueOf(MoXueYuanConstant.RUN_TIME));
+        do {
+            try {
+                //获取全部课程ID
+                List<Map<String,String>> mapList = MoXueYuanStudy.getCourse();
+                isStudyFinish = mapList.size()>0? false:true;
+                for (Map<String, String> map : mapList) {
+                    //开始课程学习
+                    MoXueYuanLog.setLog("---本次学习课程ID："+map.get(MoXueYuanEnum.COURSE_ID_NAME.getValue())+","+map.get(MoXueYuanEnum.CHAPTER_ID_NAME.getValue()),null);
+                    MoXueYuanStudy.courseStudyMethod(map.get(MoXueYuanEnum.COURSE_ID_NAME.getValue()),
+                            map.get(MoXueYuanEnum.CHAPTER_ID_NAME.getValue()));
+                }
+            }catch (FileNotFoundException e){
+                MoXueYuanLog.setLog("桌面文件没找到---"+MoXueYuanConstant.COURSE_ID_ADDRESS+e.getMessage(),e);
+            }catch (IOException e){
+                MoXueYuanLog.setLog("解析文件出错---"+e.getMessage(),e);
+            }catch (Exception e){
+                MoXueYuanLog.setLog("自动学习课程出错，请分析日志或者前往调试---",e);
             }
-        }catch (FileNotFoundException e){
-            MoXueYuanLog.setLog("桌面文件没找到---"+MoXueYuanConstant.COURSE_ID_ADDRESS+e.getMessage(),e);
-        }catch (IOException e){
-            MoXueYuanLog.setLog("解析文件出错---"+e.getMessage(),e);
-        }catch (Exception e){
-            MoXueYuanLog.setLog("自动学习课程出错，请分析日志或者前往调试---",e);
+        }while (LocalDateTime.now().isBefore(endTime) && !isStudyFinish);
+        MoXueYuanLog.setLog("学习结束---",null);
+        if (isStudyFinish){
+            MoXueYuanLog.setLog("课程已经全部学习结束",null);
         }
-        MoXueYuanLog.setLog("学习结束",null);
     }
 }
